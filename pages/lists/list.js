657d66c743f77ee14598cc3570abeb7c19716c8b
Suman Sourabh <suman.sourabh@ansrsource.com>
2022-12-17 23:25:58 +0530

    add getStaticProps function

--- pages/lists/list.js
+++ pages/lists/list.js
@@ -1,17 +1,30 @@
 import Head from "next/head";
+import Link from "next/link";
 import { useEffect, useState } from "react";
 import Footer from "../../components/Footer";
 import Navbar from "../../components/Navbar";
+import styles from "../../styles/List.module.css";
 
-const list = () => {
-  const [data, setData] = useState([]);
+export const getStaticProps = async () => {
+  const res = await fetch("https://jsonplaceholder.typicode.com/users");
+  const data = await res.json();
 
-  useEffect(() => {
-    fetch("https://jsonplaceholder.typicode.com/users")
-      .then((res) => res.json())
-      .then((json) => setData(json))
-      .catch((err) => console.log(err))
-  }, []);
+  return {
+    props: {
+      items: data
+    }
+  }
+}
+
+const list = ({ items }) => {
+  // const [data, setData] = useState([]);
+
+  // useEffect(() => {
+  //   fetch("https://jsonplaceholder.typicode.com/users")
+  //     .then((res) => res.json())
+  //     .then((json) => setData(json))
+  //     .catch((err) => console.log(err))
+  // }, []);
 
   return (
     <div>
@@ -19,11 +32,18 @@ const list = () => {
             <title>Learn Next.JS | List</title>
         </Head>
         <h1>List</h1>
-        <ul>
+        {/* <ul>
           {data.map((el => {
             return <li key={el.name}>{el.name}</li>
           }))}
-        </ul>
+        </ul> */}
+        {items.map((item => (
+          <Link href={`/lists/${item.id}`} key={item.id}>
+            <div className={styles.single}>
+              <h3>{item.name}</h3>
+            </div>
+          </Link>
+        )))}
     </div>
   )
 }
